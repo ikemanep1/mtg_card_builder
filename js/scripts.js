e82cbfef6bffcbfcaeffa56e099826b63e05d173
Isaac Esquivel-Pilloud <ike.esquivelpilloud@gmail.com>
2020-05-03 19:24:56 -0700

    add assignId function

--- js/scripts.js
+++ js/scripts.js
@@ -7,3 +7,9 @@ CardBinder.prototype.addCard = function (card) {
     card.id = this.assignId();
     this.cards.push(card);
 }
+
+CardBinder.prototype.assignId = function() {
+    this.currentId += 1;
+    return this.currentId;
+}
+
