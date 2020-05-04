632a4b935f808b706abd873848c893680bf276e5
Isaac Esquivel-Pilloud <ike.esquivelpilloud@gmail.com>
2020-05-03 19:23:14 -0700

    add card create function

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,9 @@
-function cardBinder () {
+function CardBinder () {
     var cards = [];
     var currentId = 0;
+}
+
+CardBinder.prototype.addCard = function (card) {
+    card.id = this.assignId();
+    this.cards.push(card);
 }
